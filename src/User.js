899f6428d08e501855f2f93e5507bb5fbb903982
JunreyG <143053124+RoseMel1@users.noreply.github.com>
2023-12-10 12:21:08 +0800

    Initial change

---
+++ src/User.js
@@ -0,0 +1,99 @@
+export const User = [
+    {
+      id: 1,
+      name: {
+        firstname: "Junrey",
+        lastname: "Genzon",
+      },
+      course: "BSIT",
+      yearsection: "3B",
+      username: "gif.docs",
+      password: "0000",
+      usertype: "STUDENT",
+    },
+    {
+      id: 2,
+      name: {
+        firstname: "Melrose",
+        lastname: "Valiente",
+      },
+      course: "BSIT",
+      yearsection: "3A",
+      username: "rosemel.me",
+      password: "0011",
+      usertype: "STUDENT",
+    },
+    {
+      id: 3,
+      name: {
+        firstname: "Rodel",
+        lastname: "Jawa",
+      },
+      course: "BSIT",
+      yearsection: "3B",
+      username: "rodel.g",
+      password: "1111",
+      usertype: "STUDENT",
+    },
+    {
+      id: 4,
+      name: {
+        firstname: "Jea",
+        lastname: "Oring",
+      },
+      course: "BSIT",
+      yearsection: "3A",
+      username: "jeanorene.o",
+      password: "1212",
+      usertype: "STUDENT",
+    },
+    {
+      id: 5,
+      name: {
+        firstname: "Luisa",
+        lastname: "Tinaja",
+      },
+      course: "BSIT",
+      yearsection: "3A",
+      username: "lui.t",
+      password: "1234",
+      usertype: "STUDENT",
+    },
+    {
+      id: 7,
+      name: {
+        firstname: "Jerry",
+        lastname: "Ale",
+      },
+      course: "",
+      yearsection: "",
+      username: "jerry.a",
+      password: "2323",
+      usertype: "TEACHER",
+    },
+    {
+      id: 8,
+      name: {
+        firstname: "Gabrine",
+        lastname: "Diaz",
+      },
+      course: "",
+      yearsection: "",
+      username: "gabrine.d",
+      password: "1323",
+      usertype: "TEACHER",
+    },
+    {
+      id: 9,
+      name: {
+        firstname: "Fermo",
+        lastname: "Asufra",
+      },
+      course: "",
+      yearsection: "",
+      username: "asufra.f",
+      password: "1010",
+      usertype: "TEACHER",
+    },
+    
+  ];
